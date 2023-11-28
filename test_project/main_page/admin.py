6aaee17a90730e65288547a2b1ec6d7f1a0bcf6f
bosMC <bostanAF@gmail.com>
2023-11-28 13:25:22 +0700

    add admin-sortable

--- test_project/main_page/admin.py
+++ test_project/main_page/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
+from adminsortable2.admin import SortableAdminMixin
 from main_page.models import Slider
 
 
-admin.site.register(Slider)
+@admin.register(Slider)
+class SorlableSliderAdmin(SortableAdminMixin, admin.ModelAdmin):
+    ordering = ['my_order']
+
+# admin.site.register(Slider)
--- test_project/main_page/models.py
+++ test_project/main_page/models.py
@@ -3,7 +3,11 @@ from django.db import models
 # Create your models here.
 class Slider(models.Model):
     title = models.CharField(max_length=255)
+    my_order = models.PositiveIntegerField(default=0, blank=False, null=False, editable=True)
     image = models.ImageField(upload_to='slider_images')
+
+    class Meta:
+        ordering = ['my_order']
 
     def __str__(self):
         return f'Изображение для слайдера: {self.title}'
